10cd33c87cefbcd2c370679de98bfb8affe30e7b
nicklefritz <nrcoover@gmail.com>
2025-10-08 23:52:57 -0400

    Update ErrorDefault.tsx
    Remove side margin (interfering with centering on Portfolio Page

--- react-website/src/pages/ErrorDefault.tsx
+++ react-website/src/pages/ErrorDefault.tsx
@@ -7,37 +7,63 @@ import { providePathRoot } from "../helpers/assetHelper";
 
 import globalClasses from "../global.module.css";
 import classes from "./styles/ErrorDefault.module.css";
+import { LocationContext } from "../store/location-context/LocationContext";
+import { useContext } from "react";
 
 const ErrorDefault = () => {
+	const { locationPath } = useContext(LocationContext);
+
+	const isPortfolio = locationPath.includes("portfolio");
+
+	const content = (
+		<>
+			<SideNavigation />
+			<h1>"DANGER, WILL ROBINSON!</h1>
+			<h2>Uh oh! Looks like an error occurred!</h2>
+			<ImageWrapper maxWidth={"40rem"}>
+				<img
+					className={classes.errorImage}
+					src={providePathRoot("images/defaults/ai-image_cosmic-death.png")}
+					alt="A black pug stares sadly at you..."
+				/>
+			</ImageWrapper>
+			<p>
+				Despite searching the vastness of the universe, the page you requested
+				has been lost to oblivion!
+			</p>
+			<NavLink
+				to={PATHS.Portfolio.Root}
+				className={({ isActive }) =>
+					isActive
+						? `${classes.active} ${globalClasses.iconContainer}`
+						: globalClasses.iconContainer
+				}
+				end
+			>
+				<button>Return to the known universe</button>
+			</NavLink>
+		</>
+	);
+
 	return (
 		<>
-			<main className={`${classes.errorDefault} ${classes.error}`}>
-				<SideNavigation />
-				<h1>"DANGER, WILL ROBINSON!</h1>
-				<h2>Uh oh! Looks like an error occurred!</h2>
-				<ImageWrapper maxWidth={"40rem"}>
-					<img
-						className={classes.errorImage}
-						src={providePathRoot("images/defaults/ai-image_cosmic-death.png")}
-						alt="A black pug stares sadly at you..."
-					/>
-				</ImageWrapper>
-				<p>
-					Despite searching the vastness of the universe, the page you requested
-					has been lost to oblivion!
-				</p>
-				<NavLink
-					to={PATHS.Portfolio.Root}
-					className={({ isActive }) =>
-						isActive
-							? `${classes.active} ${globalClasses.iconContainer}`
-							: globalClasses.iconContainer
-					}
-					end
-				>
-					<button>Return to the known universe</button>
-				</NavLink>
-			</main>
+			{isPortfolio ? (
+				<>
+					<div
+						className={`${isPortfolio ? "" : classes.errorDefault} ${classes.error}`}
+					>
+						{content}
+					</div>
+				</>
+			) : (
+				<>
+					<main
+						className={`${isPortfolio ? "" : classes.errorDefault} ${classes.error}`}
+					>
+						{content}
+					</main>
+				</>
+			)}
 		</>
 	);
 };
