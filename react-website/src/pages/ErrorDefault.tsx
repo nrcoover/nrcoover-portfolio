9605d0b8507ac733a52221c26e9867dafe05d897
nicklefritz <nrcoover@gmail.com>
2025-10-08 23:21:29 -0400

    Remove LoginForm and UserSettings modals from Error page
    These were conflicting with the modals in the Portfolio page, resulting in the modals not appearing in the DOM

--- react-website/src/pages/ErrorDefault.tsx
+++ react-website/src/pages/ErrorDefault.tsx
@@ -1,9 +1,5 @@
-import { useContext } from "react";
-import LoginForm from "../components/portfolio/forms/login-form/LoginForm";
-import UserSettings from "../components/portfolio/modals/user-settings/UserSettings";
 import SideNavigation from "../components/side-navigation/SideNavigation";
 import { PATHS } from "../constants/paths";
-import { AuthUserContext } from "../store/auth-user-context/AuthUserContext";
 
 import { NavLink } from "react-router-dom";
 import ImageWrapper from "../components/common/wrappers/image-wrapper/ImageWrapper";
@@ -13,13 +9,9 @@ import globalClasses from "../global.module.css";
 import classes from "./styles/ErrorDefault.module.css";
 
 const ErrorDefault = () => {
-	const { loginModal } = useContext(AuthUserContext);
-
 	return (
 		<>
 			<main className={`${classes.errorDefault} ${classes.error}`}>
-				<LoginForm ref={loginModal} />
-				<UserSettings />
 				<SideNavigation />
 				<h1>"DANGER, WILL ROBINSON!</h1>
 				<h2>Uh oh! Looks like an error occurred!</h2>
